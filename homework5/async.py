c6c1e73e4edc32bb300fbf7323b309d7cc35e69a
Pavel Ivanou <pavel_ivanou@epam.com>
2018-09-12 17:26:39 +0300

    Output sum of prime numbers
    Output sum of prime numbers in the specified range [n, m)

---
+++ homework5/async.py
@@ -0,0 +1,33 @@
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+
+N = 10 ** 2
+M = 2 * 10 ** 5
+
+
+def prime_or_zero(n):
+    """returns n if it is prime number, otherwise returns 0"""
+    if n < 2:
+        return 0
+    if n == 2:
+        return n
+    for m in range(3, n, 2):
+        if n % m == 0:
+            return 0
+    return n
+
+
+async def sum_primes(n, m):
+    loop = asyncio.get_event_loop()
+    executor = ProcessPoolExecutor()
+
+    tasks = [loop.run_in_executor(executor, prime_or_zero, i)
+             for i in range(n, m)]
+
+    completed, pending = await asyncio.wait(tasks)
+
+    return sum(t.result() for t in completed)
+
+
+if __name__ == '__main__':
+    print(asyncio.run(sum_primes(N, M)))
---
+++ homework5/concurrent_futures.py
@@ -0,0 +1,21 @@
+from concurrent.futures import ProcessPoolExecutor
+
+N = 10 ** 2
+M = 2 * 10 ** 5
+
+
+def prime_or_zero(n):
+    """returns n if it is prime number, otherwise returns 0"""
+    if n < 2:
+        return 0
+    if n == 2:
+        return n
+    for m in range(3, n, 2):
+        if n % m == 0:
+            return 0
+    return n
+
+
+if __name__ == '__main__':
+    with ProcessPoolExecutor() as executor:
+        print(sum(executor.map(prime_or_zero, range(N, M))))
