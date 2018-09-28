72ee626016b3caf832db691c4426a45b6ec721df
Pavel Ivanou <pavel_ivanou@epam.com>
2018-09-28 12:36:12 +0300

    Add some description for homeworks

---
+++ homework6/download.py
@@ -0,0 +1,10 @@
+"""
+Homework 1.
+
+Download a bunch of files
+(e.g., from https://www.python.org/downloads/source/):
+    1. using aiohttp, asyncio
+    2. using common threads
+Benchmark performance in both cases time and put results into the PR
+description.
+"""
